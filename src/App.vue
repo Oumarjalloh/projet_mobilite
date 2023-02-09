b0656a980f2007af713b5d23454ec2b7631612b3
Chad92 <116558072+Chad92@users.noreply.github.com>
2023-02-09 16:51:12 +0100

    debut responsive nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,181 @@
+<!-- <template>
+  <div>
+    <nav>
+      <ul v-if="!isMobile">
+        <li @click="toggleDropdown"><router-link to="/">Accueil</router-link></li>
+        <li @click="toggleDropdown"><router-link to="/about">A propos</router-link></li>
+        <li @click="toggleDropdown"><router-link to="/test">Test</router-link></li>
+      </ul>
+      <template v-else>
+        <div @click="toggleDropdown">
+          Menu
+          <ul v-if="showDropdown">
+            <li><router-link to="/">Accueil</router-link></li>
+            <li><router-link to="/about">A propos</router-link></li>
+            <li><router-link to="/test">Test</router-link></li>
+          </ul>
+        </div>
+      </template>
+    </nav>
+  </div>
+</template> -->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/test">Test</router-link>
+  <nav class="nav" id="mynav">
+    <div class="logo">
+      <a href="#">
+        <img src="">
+      </a>
+    </div>
+    <div class="menu-list">
+      <router-link to="/">Accueil</router-link> |
+      <router-link to="/about">A Propos</router-link> |
+      <router-link to="/test">Test</router-link>
+    </div>
+    <div class="menu">
+							<div class="line1"></div>
+							<div class="line2"></div>
+							<div class="line3"></div>
+			</div>
+
   </nav>
   <router-view/>
 </template>
+<script>
+// export default {
+//   data() {
+//     return {
+//       isMobile: window.innerWidth <= 768,
+//       showDropdown: false
+//     };
+//   },
+//   mounted() {
+//     window.addEventListener("resize", this.handleResize);
+//   },
+//   beforeUnmount() {
+//     window.removeEventListener("resize", this.handleResize);
+//   },
+//   methods: {
+//     handleResize() {
+//       this.isMobile = window.innerWidth <= 768;
+//     },
+//     toggleDropdown() {
+//       this.showDropdown = !this.showDropdown;
+//     }
+//   }
+// };
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+}
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
 nav {
   padding: 30px;
+  display: grid;
 }
 
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #4264b9;
+  text-decoration: none;
+}
+.menu div{
+	width:25px;
+	height:3px;
+	margin:5px;
+	background-color:#fff;
+	z-index:3;
+}
+
+.menu{
+	display:none;
+}
+
+@media screen and (max-width:1200px){
+	.menu-list {
+					width: 55%;
+	}
+}
+
+@media screen and (max-width:1000px){
+	.menu-list a {
+					font-size: 14px;
+	}
+}
+
+@media screen and (max-width:768px) {
+	html, body {
+					position: relative;
+	}
+
+	.menu-list {
+					position: absolute;
+					right: 0px;
+					height: 400px;
+					top: 60px;
+					background-color: #000;
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					width: 100%;
+					transform: translateY(-150%);
+					transition: transform 0.5s ease-in;
+					z-index: 1;
+	}
+
+					.menu-list li {
+									opacity: 0;
+					}
+
+	.menu {
+					display: block;
+	}
+
+	.nav {
+					background-color: #000;
+	}
+}
+	.nav-active{
+					transform: translateX(0%);
+	}
+
+	@keyframes navReveal{
+					from{
+									opacity:0;
+									transform: translateY(-50px);
+					}
+					to{
+									opacity:1;
+									transform: translateY(0px);
+					}
+	}
+
+	.toggle .line1{
+					transform: rotate(-45deg) translate(-5px, 6px);
+	}
+
+	.toggle .line2{
+					opacity:0;
+	}
+
+.toggle .line3 {
+	transform: rotate(45deg) translate(-5px, -6px);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,112 @@
 <template>
-  <div class="home">
+  <div class="hello">
+<div style="height:600px; width:800px">
+    <l-map ref="map" v-model:zoom="zoom" :center="[48.7507965, 2.2626174]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+      <div :key="marker.uuid" v-for="marker in erps">
+        <l-marker  :lat-lng="[marker.geom.coordinates[1], marker.geom.coordinates[0]]" v-on:click="Popup">
+      </l-marker>
+                <teleport to="body">
+                  <div v-if="modal" class="modal">
+                    <ul :key='erp.uuid' v-for="erp in erps">
+    <li>
+      <ul>
+        <li>{{ erp.nom }}</li>
 
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </ul>
+    </li>
+  </ul>
+
+    <button @click="open = false">Close</button>
   </div>
+                </teleport>
+      </div>
+    </l-map>
+  </div>
+
+  </div>
+<!-- Liste des erps  -->
+  <!-- <ul :key='erp.uuid' v-for="erp in erps">
+    <li>
+      <ul>
+        <li>{{ erp.nom }}</li>
+        <li>{{ erp.activite.nom }}</li>
+        <li>Latitude : {{ erp.geom.coordinates[1] }}</li>
+        <li>Longitude : {{ erp.geom.coordinates[0] }}</li>
+      </ul>
+    </li>
+  </ul> -->
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LMarker, } from "@vue-leaflet/vue-leaflet";
 export default {
-  name: 'HomeView',
+  name: 'HelloWorld',
   components: {
-    HelloWorld,
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data(){
+    return{
+      erps: null,
+      acces: null,
+      zoom: 13,
+      center: [48.7507965, 2.2626174],
+      bounds: null,
+      modal: false
+    }
+  },
+  mounted(){
+    axios
+    .get('https://acceslibre.beta.gouv.fr/api/erps/?commune=Antony&readable=true&page_size=150')
+    .then((reponse) => {
+      this.erps = reponse;
+      this.erps = this.erps.data.results
+      console.log(this.erps)
+    })
+  },
+  methods: {
+    Popup(){
+      if (this.modal== false) {
+        this.modal = true
+      }
+      else {
+        this.modal = false
+      }
+    }
   }
 }
+
 </script>
+<style>
+.modal {
+  position: fixed;
+  font-family: "Montserrat", sans-serif;
+  color: white;
+  background-color: orange;
+  border-radius: 2ch;
+  z-index: 999;
+  top: 20%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+</style>
