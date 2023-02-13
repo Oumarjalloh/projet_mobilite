95a41ecc501bbe2591a674370b532cdfbca826bf
Oumarjalloh <ojalloh38gmail.com>
2023-02-13 10:16:23 +0100

    oumar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,15 +2,14 @@
   <!-- <div class="hello">
     <h1>{{ msg }}</h1>
   </div> -->
-  <h1>Oumar</h1>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  // name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // }
 }
 </script>
 
