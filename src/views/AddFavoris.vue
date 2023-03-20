7bfb8eb740ed1ee599b477764fc456edac33b12f
Chad92 <116558072+Chad92@users.noreply.github.com>
2023-03-20 11:37:05 +0100

    responsive favoris

--- src/views/AddFavoris.vue
+++ src/views/AddFavoris.vue
@@ -1,15 +1,18 @@
 <template>
  <div class="fav-4">
+  <div class="fav-3">
+    <svg style="color: rgb(255, 113, 91);" xmlns="http://www.w3.org/2000/svg" width="52" height="35" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16"> <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" fill="#ff715b"></path> </svg>
+    <h1>Ajout aux favoris</h1>
+  </div>
 
-   <svg style="color: rgb(255, 113, 91);" xmlns="http://www.w3.org/2000/svg" width="500" height="35" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16"> <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" fill="#ff715b"></path> </svg>
-    <h1>Favoris</h1>
-    <svg style="color: rgb(15, 28, 98);" xmlns="http://www.w3.org/2000/svg" width="500" height="50" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" fill="#0f1c62"></path> </svg>
+ 
+    <svg style="color: rgb(15, 28, 98);" xmlns="http://www.w3.org/2000/svg" width="52" height="50" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" fill="#0f1c62"></path> </svg>
 </div>
-<form @submit.prevent="submitForm">
+<form @submit.prevent="submitForm" class="form">
 <input class="ip"  name="adresse" id="" placeholder="Adresse du lieu" type="adresse" v-model="adresse">
 <input class="ip"  name="nomlieu" id="" placeholder="Nom du lieu" type="nom-lieu" v-model="nomlieu">
 <textarea id="story" name="message" v-model="message"
-          rows="5" cols="33"  placeholder="Notes">
+          rows="5" cols="33"  placeholder="Notes" class="testbis">
 </textarea>
 <button class='valid-btn' type="submit"><router-link to='/favoris'>Valider</router-link></button>
 </form>
@@ -53,7 +56,6 @@ export default {
 <style>
 @media screen and (max-width:768px){
 
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap%27');
 
 :root {
 --primary: #094b65;
@@ -144,6 +146,102 @@ color: white
 .valid-btn a{
 text-decoration: none;
 color: white
+}
+}
+@media screen and (min-width: 769px){
+  :root {
+--primary: #094b65;
+}
+
+* {
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+font-family: 'Poppins', sans-serif;
+}
+
+body {
+overflow-x: hidden;
+background: #fff;
+min-height: 100vh;
+}
+
+.fav-4{
+margin: 20px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+text-align: center
+}
+
+h1{
+color: rgb(15, 28, 98);
+text-align: center;
+font-family: 'Montserrat', sans-serif;
+}
+
+
+input{
+background: #e0dede;
+justify-content: center;
+display: flex;
+margin: 10px auto;
+padding: 15px 20px 17px 20px;
+border: none;
+outline: none;
+border-radius: 5px;
+color: #0F1C62
+}
+
+.ip{
+font-size: large;
+width: 700px;
+height: 60px;
+background-color: #EBF0FF;
+}
+
+textarea{
+font-size: large;
+display: flex;
+width: 700px;
+background-color: #7692FF;
+opacity: 25%;
+border-radius: 5px;
+border: 1px solid #EBF0FF;
+margin-top: 5%;
+margin-left: auto;
+margin-right: auto;
+padding-left: 10px;
+}
+form textarea::placeholder{
+color: #0F1C62;
+z-index: 2;
+}
+.sbt{
+width: 100px;
+height: 40px;
+cursor:pointer;
+padding: 0;
+margin: 5%  0%  0%  62%;
+border-radius: 25px;
+background-color: rgb(255, 113, 91);
+}
+.valid-btn{
+ background-color: #FF715B;
+ position: relative;
+display: flex;
+margin: 10px auto;
+padding: 15px 20px 17px 20px;
+border: none;
+border-radius: 30px;
+color: white
+}
+.valid-btn a{
+text-decoration: none;
+color: white
+}
+.form{
+  margin: 30px;
 }
 }
 </style>
--- src/views/FavorisView.vue
+++ src/views/FavorisView.vue
@@ -9,13 +9,19 @@
 
 </div>
 <div class="fav-2">
-    <svg style="color: rgb(15, 28, 98);" xmlns="http://www.w3.org/2000/svg" width="52" height="35" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16"> <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" fill="#0f1c62"></path> </svg>
+  <div class="fav-3">
+    <svg class="bi-bookmark-fill" style="color: rgb(15, 28, 98);" xmlns="http://www.w3.org/2000/svg" width="52" height="35" fill="currentColor" viewBox="0 0 16 16"> <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" fill="#0f1c62"></path> </svg>
     <p>Nom du lieu <br><span>Adresse du lieu, 92000</span></p>
+  </div>
+
     <svg class="svg-1" style="color: rgb(15, 28, 98);" xmlns="http://www.w3.org/2000/svg" width="76" height="25" fill="currentColor" viewBox="0 0 16 16"> <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" fill="#0f1c62"></path> </svg>
 </div>
 <div class="fav-2">
-    <svg style="color: rgb(15, 28, 98);" xmlns="http://www.w3.org/2000/svg" width="52" height="35" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16"> <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" fill="#0f1c62"></path> </svg>
+  <div class="fav-3">
+    <svg style="color: rgb(15, 28, 98);" xmlns="http://www.w3.org/2000/svg" width="52" height="35" fill="currentColor" class="bi-bookmark-fill" viewBox="0 0 16 16"> <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" fill="#0f1c62"></path> </svg>
     <p>Nom du lieu <br><span>Adresse du lieu, 92000</span></p>
+  </div>
+
     <svg class="svg-1" style="color: rgb(15, 28, 98);" xmlns="http://www.w3.org/2000/svg" width="76" height="25" fill="currentColor"  viewBox="0 0 16 16"> <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" fill="#0f1c62"></path> </svg>
 
 </div>
@@ -64,7 +70,8 @@ export default {
 </script>
 <style>
 
-:root {
+@media  screen and (max-width: 768px) {
+  :root {
     --primary: #094b65;
 }
 
@@ -108,15 +115,107 @@ h1{
 
 .fav-2{
     display: flex;
-    margin: 0% 4% 5% 5%;
+    margin: 20px;
     background-color: #EBF0FF;
     gap: 5%;
     border-radius: 15px;
-    height: 80px;
-    padding-top: 5%;
+    padding: 10px;
+    justify-content: space-between;
+    align-items: center;
+}
+.fav-3{
+  display: flex;
+  align-items: center;
 }
 .svg-1{
     padding: 0px 0px 0px 30px;
+}
+
+.rt{
+    background-color: #EBF0FF;
+    margin: 80% 75% 0% 8%;
+    border-radius: 20px;
+}
+
+.rt-1{
+    padding: 0% 75% 0% 0%;
+}
+.return{
+    margin-left: 35px;
+    font-weight: 400;
+
+}
+span{
+  color: var(--color-titre);
+  font-weight: 200;
+}
+  
+}
+@media screen  and (min-width: 769px) {
+
+  :root {
+    --primary: #094b65;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+
+body {
+    overflow-x: hidden;
+    background: #fff;
+    max-height: 100vh;
+}
+
+.fav-1{
+  margin: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.sub-fav-1{
+    display: flex;
+    max-width: 1000px;
+    align-items: center;
+
+}
+.fav-1 img{
+  width: 25px;
+  height: 25px;
+  position: relative;
+  right: 20px
+}
+.bi-bookmark-fill{
+  width: 100px;
+}
+h1{
+    color: rgb(15, 28, 98);
+    padding-right: 95px;
+    font-family: 'Montserrat', sans-serif;
+    left: 50px;
+}
+
+
+.fav-2{
+    display: flex;
+    margin: 20px;
+    background-color: #EBF0FF;
+    gap: 20%;
+    border-radius: 15px;
+    padding: 10px;
+    justify-content: space-between;
+    align-items: center;
+}
+.fav-3{
+  display: flex;
+  align-items: center;
+}
+.svg-1{
+    padding: 0px 0px 0px 30px;
+    left: 200px;
 }
 
 .rt{
@@ -136,6 +235,7 @@ h1{
 span{
   color: var(--color-titre);
   font-weight: 200;
+}
 }
 
 
